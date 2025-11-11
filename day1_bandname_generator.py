465d3a69c4ca066b92e101c564144ec7f96a9117
Shardul Shendage <shardulshendage21@gmail.com>
2025-11-11 11:09:11 +0530

    Initial folder structure

---
+++ day1_bandname_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator.")
+city=input("what's the name of the city you grew up in?\n")
+pet=input("what is your pet's name?\n")
+print("your band name could be"+" "+city+" "+pet)
